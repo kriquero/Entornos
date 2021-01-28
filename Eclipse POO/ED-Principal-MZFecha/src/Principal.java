73e9b66161c43aadce4aa59a039c0ed2389f1b96
kriquero <quiquero201@gmail.com>
2021-01-28 12:15:14 +0100

    MZFecha bien hecho

--- Eclipse POO/ED-Principal-MZFecha/src/Principal.java
+++ Eclipse POO/ED-Principal-MZFecha/src/Principal.java
@@ -1,18 +1,57 @@
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		MZFecha f1 = new MZFecha(1,2,1990);
-		MZFecha f2 = new MZFecha(2,11,1998);
-		MZFecha f3 = new MZFecha(14,8,1975);
-		
-		f1.imprime();
-		f2.imprime();
-		f3.imprime();
-		
+		Scanner sc = new Scanner(System.in);
+int opc = 0, d, m, a;
+MZFecha f1 = new MZFecha();
+MZFecha f2 = new MZFecha();
+
+do{
+	
+	System.out.println("Indique la opcion que desee\n1. Establecer fecha\n2. Imprimir fecha\n3. Diferencia entre dos fechas");
+	
+	while(!sc.hasNextInt()) {
+		System.out.println("Mucho texto");
+		sc.next();
+	}
+	opc = sc.nextInt();
+	
+	switch(opc){
+	case 1 -> {
+		System.out.println("Introduzca un dia");
+		d = sc.nextInt();
+		System.out.println("Introduzca un mes");
+		m = sc.nextInt();
+		System.out.println("Introduzca un anyo");
+		a = sc.nextInt();
+		System.out.println("Que fecha quiere cambiar? 1 o 2");
+		opc = sc.nextInt();
+		if(opc == 1) {
+			f1.setFecha(d, m, a);
+		}else if(opc == 2) {
+			f2.setFecha(d, m, a);
+		}
+		break;
+	}
+	case 2->{
+		System.out.println("Que fecha quiere mostrar? 1 o 2");
+		opc = sc.nextInt();
+		if(opc == 1) {
+			f1.imprime();
+		}else if(opc == 2) {
+			f2.imprime();
+		}
+	}
+	case 3->{
 		System.out.println(f1.diferenciaDias(f2));
+	}
+	}
+	
+}while(opc!=0);
 		
-		
+		sc.close();
 	}
 
 }
